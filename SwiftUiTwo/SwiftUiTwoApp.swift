c31f1d2f6ad2b9e4cd651bf7b2e45b5eb89fe59a
husanbek <husanbek4030m@gmail.com>
2022-06-22 19:46:05 +0500

    Initial Commit

---
+++ SwiftUiTwo/SwiftUiTwoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUiTwoApp.swift
+//  SwiftUiTwo
+//
+//  Created by Karavella on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTwoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
